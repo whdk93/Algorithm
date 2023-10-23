1c84fce3d67bbce536661dac1e87307a5410b32b
whdk93 <110512394+whdk93@users.noreply.github.com>
2023-10-23 23:27:10 +0900

    [level unrated] Title: 짝수의 합, Time: 0.02 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if(n < 0 || n > 1000) {
+            System.out.println("range error!");
+        }else {
+            for(int i=1; i<=n; i++) {
+                if(i%2 == 0) {
+                    answer += i;
+                }
+            }   
+        }
+        
+        return answer;
+    }
+}
