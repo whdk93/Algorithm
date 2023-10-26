73bc1a1fe9bfa3c9851fced680dbcbf00db0f90c
아라 <110512394+whdk93@users.noreply.github.com>
2023-10-26 17:51:06 +0900

    [level unrated] Title: 편지, Time: 0.02 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int mwidth = message.length();
+        answer = mwidth * 2;
+        return answer;
+    }
+}
